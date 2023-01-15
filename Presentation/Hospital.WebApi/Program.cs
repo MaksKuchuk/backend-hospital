9c94edbd97d6738dbf96fe910806885cc2f64780
MaksKuchuk <maks.kuchuk.2016@gmail.com>
2023-01-16 01:22:43 +1000

    include Swagger

--- Presentation/Hospital.WebApi/Program.cs
+++ Presentation/Hospital.WebApi/Program.cs
@@ -1,21 +1,24 @@
+using DataBase;
+using DataBase.Repository;
 using Hospital.Persistence.Interfaces;
 using Hospital.Persistence.UseCases;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql($"Host=localhost;Port=5452;Database=exampleDB;Username=exampleUser;Password=examplePswd"));
-// builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql($"Host=localhost;Port=5452;Database=exampleDB;Username=exampleUser;Password=examplePswd"));
+builder.Services.AddTransient<IUserRepository, UserRepository>();
 builder.Services.AddTransient<UserService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
